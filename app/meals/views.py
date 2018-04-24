797b8b71712388c5f333d20366ae9df4283b8072
unknown <matthewacha@gmail.com>
2018-04-25 01:13:26 +0300

    ft:Get meals endpoint

--- app/meals/views.py
+++ app/meals/views.py
@@ -20,4 +20,16 @@ class add_meal(Resource):
         meals_db.append(meal_prof)
         return make_response(jsonify({'message':"Successfully added meal option"}),201)
 
+    def get(self):
+        data=request.get_json()
+        list_meals=[]
+        for meal in meals_db:
+            output={}
+            output['meal_name']=meal['details'].name
+            output['meal_price']=meal['details'].price
+            output['user_id']=meal['details'].user_id
+            output['meal_id']=meal['meal_id']
+            list_meals.append(output)
+        return jsonify({"meals":list_meals})
+
 mealapi.add_resource(add_meal, 'meals/')
