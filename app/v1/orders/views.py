a659b7d2ce91a5bea4c9db60859ff8c3fef298b5
unknown <matthewacha@gmail.com>
2018-04-25 15:22:26 +0300

    ft:write add,get endpoints for orders

--- app/v1/orders/views.py
+++ app/v1/orders/views.py
@@ -1,8 +1,20 @@
 import os
 from flask import Flask, jsonify, request, session, make_response, abort
 from flask_restful import Resource, Api
-from app.models import database, menu_db
+from app.models import database, menu_db, orders_db
 from . import orders
 
 orderapi=Api(orders)
+class make_order(Resource):
+    def post(self, meal_id):
+        menu_meal=[meal for meal in menu_db if meal['meal_id']==meal_id]
+        if len(menu_meal)>0:
+            orders_db.append(menu_meal[0])
+            return jsonify({"message":"Successfully sent"})
+        return jsonify({"message":"Not successful, try again"})
 
+class get_orders(Resource):
+    def get(self):
+        return jsonify({"orders":orders_db})
+orderapi.add_resource(make_order,'orders/<int:meal_id>')
+orderapi.add_resource(get_orders,'orders')
