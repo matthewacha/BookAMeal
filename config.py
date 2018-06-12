bf34c50bc7976126d43baa4a81013fe320fd7914
Matthew O Wacha <matthewacha@Matthews-MacBook-Pro.local>
2018-06-12 14:27:14 +0300

    [feature] Change environment to development

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ import os
 
 def create_app(dev_state):
     if dev_state == 'Development':
-        SQLALCHEMY_DATABASE_URI = "postgresql://postgres:password@localhost/bookameal"
+        SQLALCHEMY_DATABASE_URI = "postgresql://matthewacha:password@localhost/BookAMeal"
         SQLALCHEMY_TRACK_MODIFICATIONS = False
         SECRET_KEY = 'VX-4178-WD-3429-MZ-31'
     else:
         SECRET_KEY = 'VX-4178-WD-3429-MZ-31'
 
 if __name__=='__main__':
-    create_app('Dev')
+    create_app('Development')
